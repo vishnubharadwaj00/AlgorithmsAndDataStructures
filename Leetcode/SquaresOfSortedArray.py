09b1cb29c99594eac09de7cd9ba3bd820ebef946
Vishnu Bharadwaj <vishnubharadwaj@Vishnus-MacBook-Air.local>
2021-09-23 12:09:09 -0400

    modified previous solution

--- Leetcode/SquaresOfSortedArray.py
+++ Leetcode/SquaresOfSortedArray.py
@@ -1,6 +1,23 @@
-a=list(map(int,input().split()))
-b=[]
-for i in a:
-	b.append(pow(i,2))
-b.sort()
-print(b)
+def sortedSquares(nums):
+	output = [0]*len(nums)
+	write = len(nums) - 1
+	left = 0
+	right = len(nums) - 1
+
+	left_sq = nums[left]**2
+	right_sq = nums[right]**2
+
+	while(write>=0):
+		if left_sq > right_sq:
+			output[write] = left_sq
+			left +=1
+			left_sq = nums[left]**2
+		else:
+			output[write] = right_sq
+			right -=1
+			right_sq = nums[right]**2
+		write-=1
+	print(output)
+		
+nums = [-4,-4,-3]
+sortedSquares(nums)
